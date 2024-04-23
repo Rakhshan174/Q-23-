bc62d4e6de8baea21550d4ee9ac3522253c1e61d
Rakhshan174 <160410026+Rakhshan174@users.noreply.github.com>
2024-04-23 08:10:30 -0700

    Q 23 TS/JS for 45 assignment

---
+++ main.ts
@@ -0,0 +1,34 @@
+// Conditional Tests: Write a series of conditional tests. Print a statement describing each test
+// and you prediction for the results of each test.Your code took somthing like.this
+
+  let car = "subaru";
+console.log("is car == 'subaru'? predict true")
+console.log(car == 'subaru')
+
+ console.log("is car != 'honda city'? predict true")
+ console.log(car != 'honda city')
+
+console.log("is car == 'subaru'? predict false");
+console.log(car =='subaru' )
+
+console.log("is car == 'SUBARU'? predict false");
+console.log(car =='SUBARU' )
+
+console.log("is car.length == 3? predict false")
+console.log (car.length == 3)
+
+console.log("is car.length != 10? predict true")
+ console.log (car.length != 10)
+
+console.log("is 10 > 45 ? predict false");
+console.log(10 > 45);
+
+console.log("is 3 <= 2 ? predict false");
+console.log(3 <= 2);
+
+console.log("is 72 <= 63 ? predict false");
+console.log(72 <= 63);
+
+console.log("is car == 'subaru' && car.length == 6? predict true")
+console.log(car == 'subaru' && car.length == 6)
+
